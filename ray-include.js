7889f49321ee80e0ba99523b908d12e72b867f43
ray <rayvanet@gmail.com>
2021-07-13 10:00:41 +0500

    basic program working

---
+++ ray-include.js
@@ -0,0 +1,33 @@
+// #include <header_file>
+
+const fs = require('ray-fs');
+const flags = require('ray-flags');
+const {sucide} = require('sucide');
+const path = require('path');
+
+if (flags.v) sucide("v1.0.0");
+if (flags.f === undefined) sucide("No valid file provided!");
+
+const fileURI = flags.f;
+const fileContents = fs.readArray(fileURI).value;
+const includeRegex = new RegExp(/#include <[\w\.\/]+>/);
+
+
+const compiledFile = fileContents.map(line => {
+  if (includeRegex.test(line)) {
+    const includedFileURI = line
+		      .split('<')[1]
+	              .split('>')[0];
+    const includedFileContents = fs.readArray(includedFileURI).value.join('\n');
+    return includedFileContents;
+  }
+  else {
+    return line;
+  }
+}).join('');;
+
+const fileExt = path.extname(fileURI);
+const fileNameWithoutExt = path.basename(fileURI, fileExt);
+
+fs.write(`${fileNameWithoutExt}.included${fileExt}`, compiledFile);
+
